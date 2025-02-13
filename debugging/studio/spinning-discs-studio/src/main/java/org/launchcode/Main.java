34054191c9309d38605c731eeef3c7a684610099
rahinasrahman <rshinasrahman94@gmail.com>
2025-02-12 21:13:52 -0600

    Debuggin

--- debugging/studio/spinning-discs-studio/src/main/java/org/launchcode/Main.java
+++ debugging/studio/spinning-discs-studio/src/main/java/org/launchcode/Main.java
@@ -7,6 +7,7 @@ public class Main {
         // TODO: Declare and initialize a CD and a DVD object.
         CD cd = new CD(638,700,"Audio");
         DVD dvd = new DVD (3500,650,"Video");
+        dvd.getStoringType();
         // TODO: Call each CD and DVD method to verify that they work as expected.
         cd.spinDisc();
         dvd.spinDisc();
